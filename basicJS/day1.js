fb653f537a32f5d977326a1c8ac902b81fec96e4
Krish Sharma <krishsharma3355@gmail.com>
2026-04-26 23:11:49 +0530

    sum of two numbers

---
+++ basicJS/day1.js
@@ -0,0 +1,13 @@
+// sum of two numbers
+// let a = 10
+// let b = 20
+
+// let c = a + b
+// console.log(c);
+
+// concatination
+//
+//     let a= 10
+//     let b = "20"
+//     console.log(a+b);
+//
