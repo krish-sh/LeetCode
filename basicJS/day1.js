6f86e8000a8d9088d853559cef0067df89ad2080
Krish Sharma <krishsharma3355@gmail.com>
2026-04-26 23:19:04 +0530

    type coursion

--- basicJS/day1.js
+++ basicJS/day1.js
@@ -11,3 +11,15 @@
 //     let b = "20"
 //     console.log(a+b);
 //
+
+// sent a message
+let a = 100
+let b = 20
+
+// console.log("Sum of two numbers is" , a + b);
+
+// Type coursion
+
+// console.log("11"-1);
+
+
