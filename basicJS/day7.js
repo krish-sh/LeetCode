46b1f700218fe3a2ef40938ced06f7e8d038da30
Krish Sharma <krishsharma3355@gmail.com>
2026-04-27 23:06:43 +0530

    Artihmetic operator

--- basicJS/day7.js
+++ basicJS/day7.js
@@ -21,7 +21,18 @@ let b = 10
   // console.log(b);
 
   
-[a,b] = [b, a]
+// [a,b] = [b, a]
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+// Math Objects
+// console.log(Math.floor(a/b));
+// console.log(a%b)
+// 
+let c = 2325;
+
+console.log(Math.floor(c/1000));
+console.log(c%100);
+
+
