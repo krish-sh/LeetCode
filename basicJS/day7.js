e60de27027b37ec8a4400f58660b9f0556fbe12f
Krish Sharma <krishsharma3355@gmail.com>
2026-04-27 22:45:39 +0530

    swaping

---
+++ basicJS/day7.js
@@ -0,0 +1,27 @@
+// Type Casting
+// let a = prompt("What is your age");
+// a = Number(a)
+// console.log(a);
+
+// Swaping of two numbers
+
+let a = 5;
+let b = 10
+  // let temp = a
+  // a = b
+  // b = temp
+
+  // console.log(a);
+
+  // a = a + b //15
+  // b = a - b //5
+  // a = a - b //10
+
+  // console.log(a);
+  // console.log(b);
+
+  
+[a,b] = [b, a]
+
+console.log(a);
+console.log(b);
