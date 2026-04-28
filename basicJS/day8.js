5af165fd7f61de42fc4b11b61e9697f39bb5c19c
Krish Sharma <krishsharma3355@gmail.com>
2026-04-28 23:12:47 +0530

    Math operators

--- basicJS/day8.js
+++ basicJS/day8.js
@@ -11,10 +11,36 @@
 // i = i++ + --i
 // console.log(i);
 
+// let a = 11//13
+// let b = 22//24
+// let c = a + b + a++ + b++ + ++a + ++b
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-let a = 11//13
-let b = 22//24
-let c = a + b + a++ + b++ + ++a + ++b
-console.log(a);
-console.log(b);
-console.log(c);
+// Math functions
+
+// console.log(Math.round(10.6));
+// console.log(Math.ceil(10.6));
+// console.log(Math.floor(10.6));
+// console.log(Math.trunc(10.6));
+// console.log(Math.pow(10, 2));
+// console.log(Math.sqrt(100))
+// console.log(Math.cbrt(27))
+// console.log(Math.abs(-27))
+// console.log(Math.max(27, 9, 100))
+// console.log(Math.min(27, 9, 100))
+// console.log(Math.min(27, 9, 100))
+// console.log(Math.trunc(Math.random() * 900000 + 100000))
+// let a = 0.958
+// console.log(a.toFixed(2));
+
+// let a = 5;
+// let b = 7;
+
+// let area = a * b;
+// console.log(area);
+// let breath = 2 * (a + b);
+// console.log(breath);
+
+
