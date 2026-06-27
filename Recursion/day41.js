48ff115d5699b017777e67ad54199e88cf2cd6ff
Krish Sharma <krishsharma3355@gmail.com>
2026-06-27 19:13:23 +0530

    sum of n'th term

--- Recursion/day41.js
+++ Recursion/day41.js
@@ -1,12 +1,34 @@
-let n = 10;
+// let n = 10;
+// process.stdout.write(0 + " " + 1 + " ");
+// function itreate(n, first, second) {
+//   if (n == 0) return;
+//   let third = first + second;
+//   process.stdout.write(third + " ");
+//   itreate(n - 1, second, third);
+// }
+// itreate(n - 2, 0, 1);
+// Sum of nth fibinnaco with  recursion
 
-process.stdout.write(0 + " " + 1 + " ");
+// let n = 5;
+// let first = 0;
+// let second = 1;
 
-function itreate(n, first, second) {
-  if (n == 0) return;
-  let third = first + second;
-  process.stdout.write(third + " ");
-  itreate(n - 1, second, third);
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += second;
+//   let third = first + second;
+//   first = second;
+//   second = third;
+// }
+// console.log(sum);
+
+function iterate(n) {
+  if (n == 0 || n == 1) return n;
+
+  return iterate(n - 1) + iterate(n - 2);
 }
 
-itreate(n - 2, 0, 1);
+let n = 4;
+
+console.log(iterate(n));
