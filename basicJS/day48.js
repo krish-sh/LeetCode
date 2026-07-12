0576ff01349f7eeb61df5679c643abb30271c679
Krish Sharma <krishsharma3355@gmail.com>
2026-07-12 19:18:16 +0530

    change the formula of finding mid

--- basicJS/day48.js
+++ basicJS/day48.js
@@ -1,10 +1,11 @@
 let nums = [2, 5, 8, 9, 12, 18, 20, 25, 67, 101, 125];
 
-target = 30
+target = 67
 let i = 0 
 let j = nums.length-1
 while(i <= j){
-     let k = Math.floor((i+j)/2)
+   // let k = Math.floor(i+j)/2  this was only compatible in some languages but 
+     let k = Math.floor(i+(j-i)/2) //  this one is used in every language so i prefer this
      if(nums[k] === target){
         console.log("Target: ", nums[k], "on Index: ", k);
         break
@@ -12,10 +13,6 @@ while(i <= j){
    if(nums[k] < target){
     i = k + 1
    }else j = k - 1
-}
-if(i >= j){
-    console.log("Element not found!");
-    
 }
 
 
