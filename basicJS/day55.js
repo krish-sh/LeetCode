887fdfedb16f3e88f18396030adae38b8bb30765
Krish Sharma <krishsharma3355@gmail.com>
2026-08-08 13:36:45 +0530

    count element greater than prev average

---
+++ basicJS/day55.js
@@ -0,0 +1,16 @@
+// Count element grater than pervious Average
+
+nums = [100, 200, 150, 300]
+
+let sum = 0
+let count = 0
+
+for(let i= 1; i<nums.length; i++){
+    sum += nums[i-1]
+    let average = sum / i
+    if(nums[i] > average){
+        count++
+    }
+}
+
+console.log(count);
