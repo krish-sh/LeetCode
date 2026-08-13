4241891e92988358ad3b15cfae202ee2379fdf04
Krish Sharma <krishsharma3355@gmail.com>
2026-08-13 17:03:00 +0530

    revrese traingle and diamond pattern and inverted pyramind

--- Pattern-Programming/day59.js
+++ Pattern-Programming/day59.js
@@ -53,46 +53,112 @@ for (let i = 1; i <= no; i++) {
 }
 
 // * * * * *
-// * * * * 
-// * * * 
-// * * 
-// * 
+// * * * *
+// * * *
+// * *
+// *
 
-for(let i = 1; i<= no; i++){
-    for(let j = no; j >=i; j--){
-        // process.stdout.write("* ")
-    }
-    // console.log(" ");
+for (let i = 1; i <= no; i++) {
+  for (let j = no; j >= i; j--) {
+    // process.stdout.write("* ")
+  }
+  // console.log(" ");
 }
 
-// 1 2 3 4 5 
-// 1 2 3 4 
-// 1 2 3 
-// 1 2 
-// 1  
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
 
-for(let i = 0; i < no; i++){
-    for(let j = 1; j <= no- i; j++){
-        // process.stdout.write(j + " ")
-    }
-    // console.log(" ");
+for (let i = 0; i < no; i++) {
+  for (let j = 1; j <= no - i; j++) {
+    // process.stdout.write(j + " ")
+  }
+  // console.log(" ");
 }
 
 //         *
-//       * * *  
+//       * * *
 //     * * * * *
 //   * * * * * * *
 // * * * * * * * * *
 
-for(let i = 0; i < no; i++){
-    for(let j = 1; j <= no - i - 1; j++){
-        process.stdout.write(" ")
-    }
-    for(let k = 1; k <= 2*i+1; k++){
-        process.stdout.write("*")
-    }
-    console.log("");
-    
+for (let i = 0; i < no; i++) {
+  for (let j = 1; j <= no - i - 1; j++) {
+    // process.stdout.write(" ");
+  }
+  for (let k = 1; k <= 2 * i + 1; k++) {
+    // process.stdout.write("*");
+  }
+  //   console.log("");
 }
 
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
 
+for (let i = 0; i < no; i++) {
+  for (let j = 1; j <= i; j++) {
+    // process.stdout.write(" ")
+  }
+  for (let k = 1; k <= 2 * no - (2 * i + 1); k++) {
+    // process.stdout.write("*")
+  }
+  // console.log("");
+}
+
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+
+for (let i = 0; i < no; i++) {
+  for (let j = 1; j <= no - i - 1; j++) {
+    // process.stdout.write(" ")
+  }
+  for (let k = 1; k <= 2 * i + 1; k++) {
+    // process.stdout.write("*")
+  }
+  // console.log("");
+}
+for (let i = 0; i < no; i++) {
+  for (let j = 1; j <= i; j++) {
+    // process.stdout.write(" ")
+  }
+  for (let k = 1; k <= 2 * no - (2 * i + 1); k++) {
+    // process.stdout.write("*")
+  }
+  // console.log("");
+}
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+for (let i = 0; i <= no; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log("");
+}
+for (let i = 1; i < no; i++) {
+  for (j = 1; j <= no - i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(""); 
+}
