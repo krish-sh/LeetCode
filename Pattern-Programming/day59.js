dc31a563f2a6b8eddc8216be9f04d0bbc1636996
Krish Sharma <krishsharma3355@gmail.com>
2026-08-13 16:19:20 +0530

    Pyramid Traingle and reverse right angle traingle

--- Pattern-Programming/day59.js
+++ Pattern-Programming/day59.js
@@ -23,7 +23,7 @@ for (let i = 1; i <= no; i++) {
   for (let j = 1; j <= i; j++) {
     // process.stdout.write("* ");
   }
-//   console.log(" ");
+  //   console.log(" ");
 }
 
 // 1
@@ -32,10 +32,67 @@ for (let i = 1; i <= no; i++) {
 // 1 2 3 4
 // 1 2 3 4 5
 
-for(let i = 1; i <= no; i++){
-    for(let j = 1; j <= i; j++){
-        process.stdout.write(j + " ")
+for (let i = 1; i <= no; i++) {
+  for (let j = 1; j <= i; j++) {
+    // process.stdout.write(j + " ")
+  }
+  // console.log(" ");
+}
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+for (let i = 1; i <= no; i++) {
+  for (let j = 1; j <= i; j++) {
+    // process.stdout.write(i + " ")
+  }
+  // console.log(" ");
+}
+
+// * * * * *
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+for(let i = 1; i<= no; i++){
+    for(let j = no; j >=i; j--){
+        // process.stdout.write("* ")
     }
-    console.log(" ");
+    // console.log(" ");
+}
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1  
+
+for(let i = 0; i < no; i++){
+    for(let j = 1; j <= no- i; j++){
+        // process.stdout.write(j + " ")
+    }
+    // console.log(" ");
+}
+
+//         *
+//       * * *  
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+
+for(let i = 0; i < no; i++){
+    for(let j = 1; j <= no - i - 1; j++){
+        process.stdout.write(" ")
+    }
+    for(let k = 1; k <= 2*i+1; k++){
+        process.stdout.write("*")
+    }
+    console.log("");
     
 }
+
+
