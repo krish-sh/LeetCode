f0722c6d59ec46d601b5af3cc077d10101e60ad9
Krish Sharma <krishsharma3355@gmail.com>
2026-08-19 20:30:41 +0530

    hashing using ,map

--- Hashing/day65.js
+++ Hashing/day65.js
@@ -26,3 +26,15 @@
 // console.log(str);
 
 
+//Hash using map
+
+let arr = [1,2,3,6,3,3,2,6]
+
+let map = new Map()
+
+for(let i = 0; i < arr.length; i++){
+    map.set(arr[i], (map.get(arr[i]) || 0) +1)
+}
+console.log(map);
+
+
