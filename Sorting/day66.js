17dca1d048a14e6d794a63e3715735b5bf7c6c81
Krish Sharma <krishsharma3355@gmail.com>
2026-08-21 06:29:19 +0530

    Selection and bubble sorting

---
+++ Sorting/day66.js
@@ -0,0 +1,28 @@
+// selection sort
+// let nums = [13,46,24,52,20,9]
+// for(let i = 0 ; i< nums.length-1; i++){
+//     let min = i
+//     for(let j =i+1; j<nums.length;j++){
+//       if(nums[j] < nums[min]){
+//         min = j
+//       }
+//     }
+//     [nums[i], nums[min]] = [nums[min], nums[i]]
+// }
+// console.log(nums);
+
+// Bubble sort
+
+// let nums = [13, 46, 24, 52, 20, 9];
+// let n = nums.length - 1;
+// console.log(n);
+
+// for (i = n; i >= 1; i--) {
+//   for (let j = 0; j < i; j++) {
+//     if (nums[j] > nums[j + 1]) {
+//       [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+//     }
+//   }
+// }
+// console.log(nums);
+
